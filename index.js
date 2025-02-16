9a869e349e7bc33116f859227f684db366abf83c
ShivaAdhikari7 <shivaadhikari499@gmail.com>
2025-02-16 23:55:09 +0545

    make minor changes regarding environment variables

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
 app.use(cors());
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 const MIN_NUMBER = 0;
 
 const romanNumerals = [
